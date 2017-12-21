c544ed451ecaa9bfb6c86de5bc9456d0a416c89f
spiralofhope <spiralofhope@spiralofhope.com>
2017-12-21 10:12:49 -0800

    4.3.0_r4

--- LooksBetterOnYou.lua
+++ LooksBetterOnYou.lua
@@ -1,15 +1,15 @@
--- Project: Looks Better On You r3
+-- Project: Looks Better On You r4
 -- File: LooksBetterOnYou.lua
--- Last Modified: 2012-03-11T03:01:54Z
+-- Last Modified: 2012-03-11T18:55:26Z
 -- Author: msaint
 -- Desc: Lets your alts use the dressing room.
 
 
 local OUR_NAME = "LooksBetterOnYou"
-local OUR_VERSION = string.match("r3", "([%d\.]+)")
+local OUR_VERSION = string.match("r4", "([%d\.]+)")
 OUR_VERSION = tonumber(OUR_VERSION) or 2
-local DEBUG = nil
-local debug = DEBUG and function(s) DEFAULT_CHAT_FRAME:AddMessage(s, 1, 0, 0) end or function() return end  
+local DEBUG = true
+local debug = DEBUG and function(s) DEFAULT_CHAT_FRAME:AddMessage("LBOY: "..s, 1, 0, 0) end or function() return end  
 
 -- **Constants -- Until Blizz changes them
 local raceID = {
@@ -68,8 +68,10 @@ local GetItemInfo = GetItemInfo
 
 -- **Locals
 local events = {}
-local eFrame
 local lbDB, playerDB
+local eFrame = LooksBetterEventFrame or CreateFrame("Frame", "LooksBetterEventFrame", UIParent)
+-- local lbButton = LooksBetterButton or CreateFrame("Button", "LooksBetterButton", UIParent)
+local currentAlt = {realm = GetRealmName(), name = UnitName('player')}
 
 -- **Local functions
 local function chatMsg(s, r, g, b)
@@ -83,28 +85,8 @@ local function freshenItemData()
       for name, altinfo in pairs(realmDB) do
          debug(name, altinfo.race, altinfo.sex)
          for _, item in pairs(altinfo.equip) do
---          local itemID = string.match(link, "%d+", 1)
             GetItemInfo(item)
          end
-      end
-   end
-end
-
-local function listSavedAlts()
--- Lists alts on this realm for which we have data
-   for realm, realmDB in pairs(lbDB) do
-      for name, info in pairs(realmDB) do
-         chatMsg(realm.."."..name..", "..info.race..", "..info.sex)
-      end
-   end
-end
-
-local function slashCmdParser(name)
-   if type(name) == "string" then
-      if name == "list" then
-         listSavedAlts()
-      else
-         lb:loadAltDressup(name)
       end
    end
 end
@@ -119,20 +101,87 @@ local function initDB()
    playerDB = lbDB[realm][player] 
 end
 
+local function altIsPlayer()
+   return currentAlt.name == UnitName("player") and currentAlt.realm == GetRealmName()
+end
+
+local function listSavedAlts()
+-- Lists alts on this realm for which we have data
+   for realm, realmDB in pairs(lbDB) do
+      for name, info in pairs(realmDB) do
+         chatMsg(realm.."."..name..", "..info.race..", "..info.sex)
+      end
+   end
+end
+
+local function setCurrentAlt(name, realm)
+   if not name then
+      return
+   elseif not realm then
+      realm, name = strsplit(".", name)
+      if not name then
+         name = realm
+         realm = GetRealmName()
+         -- See if the name exists on the current realm, otherwise search for it.
+         if not (lbDB[realm] and lbDB[realm][name]) then
+            for realmname, realmDB in pairs(lbDB) do
+               if realmDB[name] then
+                  realm = realmname
+                  break
+               end
+            end
+         end
+      end
+   end
+   local alt = lbDB and lbDB[realm] and lbDB[realm][name] or nil
+   if alt and alt.race and alt.sex then
+      currentAlt.name = name
+      currentAlt.realm = realm
+      return name, realm
+   end
+end
+
+local function slashCmdParser(name)
+   if type(name) == "string" then
+      if name == "list" then
+         listSavedAlts()
+      elseif name == "reset" then
+         setCurrentAlt(UnitName("player"))
+         if DressUpFrame:IsShown() then
+            DressUpModel:SetUnit("player")
+         end
+      else
+         if setCurrentAlt(name) then
+            if ( not DressUpFrame:IsShown() ) then
+      			ShowUIPanel(DressUpFrame)
+      		end
+            DressUpModel:SetUnit("player")
+         end
+      end
+   end
+end
+
+local function onDress()
+   debug("onDress was called!")
+   if not altIsPlayer() then
+      lb:loadAltDressup()
+   end
+end
+
 -- **Event handling and triggers
 function events:ADDON_LOADED(...)
    local addonName = ...
-   chatMsg(addonName)
    if (addonName == OUR_NAME) then
       self:UnregisterEvent("ADDON_LOADED")
       self:RegisterEvent("PLAYER_LOGIN")
+      hooksecurefunc(DressUpModel, "SetUnit", onDress)
+      hooksecurefunc(DressUpModel, "Dress", onDress)      
       initDB()   
       -- Make sure the client loads item data for everything our alts have equipped
       freshenItemData()
       -- Add Slash Commands
       SlashCmdList["LOOKSBETTER"] = slashCmdParser
       SLASH_LOOKSBETTER1 = "/lboy"
-
       self.ADDON_LOADED = nil
    end
 end
@@ -144,7 +193,8 @@ function events:PLAYER_LOGIN()
    chatMsg(L["\"Looks Better On You!\" loaded."])
    chatMsg(L["  Type '/lboy AltName' to load AltName in the Dressing Room."])
    chatMsg(L["  Type '/lboy list' to print a list of available alts."])
-   chatMsg(L["  If you have an alt named 'list' then that sucks for you."])
+   chatMsg(L["  Type '/lboy reset' to reset to the current player."])
+   chatMsg(L["  If you have an alt named 'list' or 'reset' then that sucks for you."])
    self:UNIT_INVENTORY_CHANGED("player")    
 end   
 
@@ -165,7 +215,6 @@ end
 do
 -- After this section, registered events will be redirected to event:EVENT_NAME(...)
 --
-   eFrame = LooksBetterEventFrame or CreateFrame("Frame", "LooksBetterEventFrame", UIParent)
    do
    -- Think of this as an OnLoad function:
       eFrame:SetScript("OnEvent", function(self, event, ...)
@@ -244,28 +293,21 @@ end
 -- **Public methods
 
 
-function lb:loadAltDressup(name)
--- Would be a good idea to have a file scope var to see if items have been
--- initialized.  If not, call init function and set a timer to re-run this
--- function. 
-   local realm, name = strsplit(".", name)
-   if not name then
-      name = realm
-      realm = GetRealmName()
-   end
+function lb:loadAltDressup(name, realm)
+   local name, realm = currentAlt.name, currentAlt.realm
    local alt = lbDB and lbDB[realm] and lbDB[realm][name] or nil
    if alt and alt.race and alt.sex then
-      if ( not DressUpFrame:IsShown() ) then
-			ShowUIPanel(DressUpFrame)
-			DressUpModel:SetUnit("player")
-		end
       DressUpModel:SetCustomRace(raceID[string.upper(alt.race)], alt.sex - 2)
       DressUpModel:Undress() -- Must be after SetCustomRace
       for _, item in pairs(alt.equip) do
          if IsEquippableItem(item) and not ((select(9, GetItemInfo(item))) == "INVTYPE_BAG") then
-            debug((select(1,GetItemInfo(item))))
+            --debug((select(1,GetItemInfo(item))))
             DressUpModel:TryOn("item:"..item)
          end
       end
    end
 end
+
+-- LooksBetter:lbButton_OnClick()
+-- 
+-- end
