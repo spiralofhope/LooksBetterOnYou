539b51d8fab5a21909fe97e25799d0080f728564
spiralofhope <spiralofhope@spiralofhope.com>
2017-12-21 10:15:56 -0800

    4.3.0_1.03-beta

--- LooksBetterOnYou.lua
+++ LooksBetterOnYou.lua
@@ -1,4 +1,4 @@
--- Project: Looks Better On You r10
+-- Project: Looks Better On You 1.03-beta
 -- File: LooksBetterOnYou.lua
 -- Last Modified: 2012-03-14T00:16:02Z
 -- Author: msaint
@@ -6,7 +6,7 @@
 
 
 local OUR_NAME = "LooksBetterOnYou"
-local OUR_VERSION = string.match("r10", "([%d\.]+)")
+local OUR_VERSION = string.match("1.03-beta", "([%d\.]+)")
 OUR_VERSION = tonumber(OUR_VERSION) or 2
 local DEBUG = nil
 local debug = DEBUG and function(s) DEFAULT_CHAT_FRAME:AddMessage("LBOY: "..s, 1, 0, 0) end or function() return end  
